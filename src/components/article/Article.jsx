33b5f87a4f518873b42f98ad56ec4fb19e30ff3e
Enesi Wise <enesiwise@gmail.com>
2023-08-04 14:59:17 +0100

    .

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./article.css";
+// import {
+//   blog01,
+//   blog02,
+//   blog03,
+//   blog04,
+//   blog05,
+// } from "../../containers/blog/imports";
+
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
